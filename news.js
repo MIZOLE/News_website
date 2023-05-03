d004ef7617a3cac20ead45b69c3cfcbde1ebbec0
Lulama Komana <komanalulama20@gmail.com>
2023-05-03 15:00:54 +0200

    added title, author and description api's

--- news.js
+++ news.js
@@ -1,6 +1,8 @@
 async function GetNews(){
 
-await fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=ef2320f48834438fb872ab95e0f3d780')
+            // let search = document.get
+
+await fetch('https://newsapi.org/v2/everything?q="KANYEwest"&apiKey=ef2320f48834438fb872ab95e0f3d780')
 
 .then(d => d.json())
 .then(response  => {
@@ -15,10 +17,13 @@ await fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=ef2320f48834438f
         try {
         show.innerHTML += `
         <div class="card">
-            <div class="card-body">
+        <div class="card-body">
             <h2>${response.articles[i].title}</h2>
             <h3>${response.articles[i].author}</h3>
             <h4>${response.articles[i].description}</h4>
+            <H5><button>${response.articles[i].url}</button>
+            <h4>
+
             </div>
         </div>
         `
