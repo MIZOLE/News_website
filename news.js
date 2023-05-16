bb0dd91dbd29e737f77143ff7de0a85d057cdd1a
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-05-16 09:43:25 +0200

    news website changes

--- news.js
+++ news.js
@@ -1,5 +1,5 @@
 // function searchBar(query) {
-//     searchUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=c344e5ff138541a2915af354552729e1`
+//     searchUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=acf13d67f2334b1485d1ae2961fe1f92`
     
 //     fetch(searchUrl)
 //         .then(res => res.json())
@@ -21,11 +21,53 @@
 // document.getElementById("date").innerHTML = date
 
 
+function GetGeneral() {
+
+    fetch('https://newsapi.org/v2/everything?q="general"&apiKey=57d0e96d029f4224a5dee612b4b6fef1')
+
+    .then(d => d.json())
+    .then(response => {
+        console.log(response)
+        let limit = response.articles.slice(0, 20)
+        // console.log(limit)
+        for (var i = 0; i <= limit.length - 1; i++) {
+            // console.log(limit.title)
+
+            let show = document.getElementById("showing5");
+
+            try {
+                show.innerHTML += `
+
+        <div class="card mt-3">
+        <div class="card-body">
+        <img src=${response.articles[i].urlToImage} class="images">
+        <h2>${response.articles[i].title}</h2>
+        <h3>${response.articles[i].author}</h3>
+        <p>${response.articles[i].description}</p>
+        <p>${response.articles[i].publishedAt}</p>
+        
+        <h6>If you want to read more, visit the following link
+        <a id="link" href=${response.articles[i].url}>READ MORE</a> </h6>         
+        </div>
+    </div>
+    `
+            }
+            catch (err) {
+                // console.log(err)
+            }
+        }
+
+    })
+}
+GetGeneral()
 
 
-function GetSports() {
 
-        fetch('https://newsapi.org/v2/everything?q="sports"&apiKey=7d881412fd464bbe97300934cfe1660a')
+
+
+function GetHealth() {
+
+        fetch('https://newsapi.org/v2/everything?q="health"&apiKey=57d0e96d029f4224a5dee612b4b6fef1')
 
         .then(d => d.json())
         .then(response => {
@@ -47,8 +89,8 @@ function GetSports() {
             <h3>${response.articles[i].author}</h3>
             <p>${response.articles[i].description}</p>
             <p>${response.articles[i].publishedAt}</p>
-            <a href=${response.articles[i].url}>Read more</a>            
-            </div>
+            <h6>If you want to read more, visit the following link
+            <a id="link" href=${response.articles[i].url}>READ MORE</a> </h6>            </div>
         </div>
         `
                 }
@@ -59,11 +101,11 @@ function GetSports() {
 
         })
 }
-GetSports()
+GetHealth()
 
- function GetTech() {
+function GetTech() {
 
- fetch('https://newsapi.org/v2/everything?q="tech"&apiKey=7d881412fd464bbe97300934cfe1660a')
+ fetch('https://newsapi.org/v2/everything?q="tech"&apiKey=57d0e96d029f4224a5dee612b4b6fef1')
 
         .then(d => d.json())
         .then(response => {
@@ -87,8 +129,8 @@ GetSports()
                     <h3>${response.articles[i].author}</h3>
                     <p>${response.articles[i].description}</p>
                     <p>${response.articles[i].publishedAt}</p>
-                    <a href=${response.articles[i].url}>Read more</a>   
-                    </div>
+                    <h6>If you want to read more, visit the following link
+                    <a id="link" href=${response.articles[i].url}>READ MORE</a> </h6>                    </div>
                 </div>
                     `
                 }
@@ -103,7 +145,7 @@ GetTech()
 
 function GetBusiNews() {
 
-    fetch('https://newsapi.org/v2/everything?q="business"&apiKey=7d881412fd464bbe97300934cfe1660a')
+    fetch('https://newsapi.org/v2/everything?q="business"&apiKey=57d0e96d029f4224a5dee612b4b6fef1')
 
         .then(d => d.json())
         .then(response => {
@@ -127,7 +169,8 @@ function GetBusiNews() {
             <h3>${response.articles[i].author}</h3>
             <p>${response.articles[i].description}</p>
             <p>${response.articles[i].publishedAt}</p>
-            <a href=${response.articles[i].url}>Read more</a>   
+            <h6>If you want to read more, visit the following link
+            <a id="link" href=${response.articles[i].url}>READ MORE</a> </h6>
             </div>
         </div>
         `
@@ -140,3 +183,66 @@ function GetBusiNews() {
         })
 }
 GetBusiNews()
+
+
+function GetEntertainment() {
+
+    fetch('https://newsapi.org/v2/everything?q="entertainment"&apiKey=57d0e96d029f4224a5dee612b4b6fef1')
+
+    .then(d => d.json())
+    .then(response => {
+        console.log(response)
+        let limit = response.articles.slice(0, 20)
+        // console.log(limit)
+        for (var i = 0; i <= limit.length - 1; i++) {
+            // console.log(limit.title)
+
+            let show = document.getElementById("showing4");
+
+            try {
+                show.innerHTML += `
+
+        <div class="card mt-3">
+        <div class="card-body">
+        <img src=${response.articles[i].urlToImage} class="images">
+        <h2>${response.articles[i].title}</h2>
+        <h3>${response.articles[i].author}</h3>
+        <p>${response.articles[i].description}</p>
+        <p>${response.articles[i].publishedAt}</p>
+        <h6>If you want to read more, visit the following link
+        <a id="link" href=${response.articles[i].url}>READ MORE</a> </h6>
+        </div>
+    </div>
+    `
+            }
+            catch (err) {
+                // console.log(err)
+            }
+
+        }
+        let mybutton = document.getElementById("myBtn");
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction(){
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // function topfunction() {
+        //     document.body.scrollTop = 0;
+        //     document.documentElement.scrollTop = 0;
+        // }
+
+        mybutton.addEventListener("click", () =>{
+            window.scrollTo({
+                top: 0,
+                behaviour: "smooth"
+            })
+        })
+
+    })
+}
+GetEntertainment()
